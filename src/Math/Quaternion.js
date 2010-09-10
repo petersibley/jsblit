0ad297b5dfd08fa9eb285635176b3e69ba83088a
Mark Dawson <markdawson@live.com>
2010-09-09 22:52:59 -0700

    Added Quaternion class

---
+++ src/Math/Quaternion.js
@@ -0,0 +1,282 @@
+//For more information I would recommend "Essential Mathematics For Games", Van Verth, Bishop
+
+/**
+* Quaternion
+* @constructor
+*/
+function Quaternion(w, x, y, z) {
+    this.w = w;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+/**
+* Creates a Quaternion from a rotation matrix
+* @param {Matrix3x3} m rotation matrix
+* @return {Quaternion}
+*/
+Quaternion.fromRotationMatrix = function (m) {
+    var trace, temp, result, largestIndex;
+    
+    //See: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+    
+    trace = m.m11 + m.m22 + m.m33;
+    if (MathHelper.abs(trace) > MathHelper.zeroTolerance)
+    {
+        result = new Quaternion(0, 0, 0, 0);
+        result.w = MathHelper.sqrt(trace + 1) * 0.5;
+        temp = 1.0 / (4 * result.w);
+        result.x = (m.m32 - m.m23) * temp;
+        result.y = (m.m13 - m.m31) * temp;
+        result.z = (m.m21 - m.m12) * temp;
+    }
+    else
+    {
+        largestIndex = 0;
+        if (m.m22 > m.m11)
+        {
+            largestIndex = 1;
+            if (m.m33 > m.m22)
+            {
+                largestIndex = 2;
+            }
+        }
+        else if (m.m33 > m.m11)
+        {
+            largestIndex = 2;
+        }
+        
+        if (largestIndex === 0)
+        {
+            temp = 1.0 / (4 * this.x);
+            return new Quaternion((m.m32 - m.m23) * temp,
+                                  0.5 * MathHelper.sqrt(m.m11 - m.m22 - m.m33 + 1),
+                                  (m.m12 + m.m21) * temp,
+                                  (m.m13 + m.m31) * temp);
+        }
+        else if (largestIndex === 1)
+        {
+            temp = 1.0 / (4 * this.y);
+            return new Quaternion((m.m13 - m.m31) * temp,
+                                  (m.m12 + m.m21) * temp,
+                                  0.5 * MathHelper.sqrt(m.m22 - m.m11 - m.m33 + 1),
+                                  (m.m23 + m.m32) * temp);
+        }
+        else
+        {
+            temp = 1.0 / (4 * this.z);
+            return new Quaternion((m.m21 - m.m12) * temp,
+                                  (m.m13 + m.m31) * temp,
+                                  (m.m32 + m.m23) * temp,
+                                  0.5 * MathHelper.sqrt(m.m33 - m.m11 - m.m22 + 1));
+        }
+    }
+    
+    return result;
+};
+
+/**
+* Creates a Quaternion from an axis and an angle
+* @param {Vector3} axis The rotation axis, must be a unit vector
+* @param {Number} angle An angle in radians.  A positive angle will rotate anticlockwise around the axis
+* @return {Quaternion}
+*/
+Quaternion.fromAxisAngle = function (axis, angle) {
+    var halfAngle, s;
+    halfAngle = 0.5 * angle;
+    s = MathHelper.sin(halfAngle);
+    return new Quaternion(MathHelper.cos(halfAngle), axis.x * s, axis.y * s, axis.z * s);
+};
+
+/**
+* Returns a quaternion that has been slerped between source and target by t amount
+* @param {Number} t A value between 0.0 and 1.0 inclusive
+* @param {Quaternion} source The starting quaternion value
+* @param {Quaternion} target The target quaternion value
+* @return {Quaternion}
+*/
+Quaternion.slerp = function (t, source, target) {
+    var cos, angle, sin, invSin, a, b;
+    
+    if (t === 0.0)
+    {
+        return source;
+    }
+    if (t === 1.0)
+    {
+        return target;
+    }
+    
+    cos = source.dot(target);
+    angle = MathHelper.acos(cos);
+    if (MathHelper.abs(angle) >= MathHelper.zeroTolerance)
+    {
+        sin = MathHelper.sin(angle);
+        invSin = 1.0 / sin;
+        a = MathHelper.sin((1.0 - t) * angle) * invSin;
+        b = MathHelper.sin(t * angle) * invSin;
+        return source.multiplyScalar(a).add(target.multiplyScalar(b));
+    }
+    
+    return source;
+};
+
+Quaternion.prototype = 
+{
+    /**
+    * Returns the dot product of two Quaternions
+    * @param {Quaternion} q input quaternion
+    * @return {Number}
+    */
+    dot: function (q) {
+        return this.w * q.w + this.x * q.x + this.y * q.y + this.z * q.z;
+    },
+    
+    /**
+    * Calculates the length of the Quaternion
+    * @return {Number}
+    */
+    length: function () {
+        return MathHelper.sqrt(this.w * this.w + this.x * this.x + this.y * this.y + this.z * this.z);
+    },
+    
+    /**
+    * Creates a unit length version of the Quaternion
+    * @return {Quaternion}
+    */
+    normalize: function () {
+        var length, inverseLength;
+        
+        length = this.length();
+        if (length < MathHelper.zeroTolerance) {
+            return new Quaternion(0.0, 0.0, 0.0, 0.0);
+        }
+
+        inverseLength = 1.0 / length;
+        return new Quaternion(this.w * inverseLength,
+                              this.x * inverseLength,
+                              this.y * inverseLength,
+                              this.z * inverseLength);
+    },
+    
+    /**
+    * Returns the inverse of the calling quaternion
+    * @return {Quaternion} If the Quaternion cannot be inversed, a Quaternion with x,y,z,w == 0.0 is returned
+    */
+    inverse: function () {
+        var norm, invNorm;
+        
+        norm = this.w * this.w + this.x * this.x + this.y * this.y * this.z * this.z;
+        if (MathHelper.abs(norm) > MathHelper.zeroTolerance)
+        {
+            invNorm = 1.0 / norm;
+            return new Quaternion(this.w * invNorm,
+                                  -this.x * invNorm,
+                                  -this.y * invNorm,
+                                  -this.z * invNorm);
+        }
+        return new Quaternion(0.0, 0.0, 0.0, 0.0);
+    },
+    
+    /**
+    * Returns the conjugate of the quaternion
+    * @return {Quaternion}
+    */
+    conjugate: function () {
+        return new Quaternion(this.w, -this.x, -this.y, -this.z);
+    },
+    
+    /**
+    * Applies the quaternion rotation to the input vector and returns the result
+    * @param {Vector3} v input vector to be rotated
+    * @return {Vector3} The rotated vector
+    */
+    transform: function (v) {
+        
+        //See Bishop, Van Verth (make sure to look at the errata)
+        var p, d, c;
+        d = 2.0 * (this.x * v.x + this.y * v.y + this.z * v.z);
+        c = 2.0 * this.w;
+        p = c * this.w - 1.0;
+        return new Vector3(p * v.x + d * this.x + c * (this.y * v.z - this.z * v.y),
+                           p * v.y + d * this.y + c * (this.z * v.x - this.x * v.z),
+                           p * v.z + d * this.z + c * (this.x * v.y - this.y * v.x));
+    },
+    
+    add: function (q) {
+        return new Quaternion(this.w + q.w, this.x + q.x, this.y + q.y, this.z + q.z);
+    },
+    
+    /**
+    * Returns a Quaternion representing the result of multiplying the calling Quaternion by Quaternion q
+    * like q2 * q1 where q1 is applied before q2
+    * @param {Quaternion} q input quaternion
+    * @return {Quaternion}
+    */
+    multiply: function (q) {
+        return new Quaternion(this.w * q.w - this.x * q.x - this.y * q.y - this.z * q.z,
+                              this.y * q.z - this.z * q.y + this.w * q.x + q.w * this.x,
+                              this.z * q.x - this.x * q.z + this.w * q.y + q.w * this.y,
+                              this.x * q.y - this.y * q.x + this.w * q.z + q.w * this.z);
+    },
+    
+    /**
+    * Multiplies each element in the Quaternion by the scalar f
+    * @param {Number} f input scalar
+    * @return {Quaternion}
+    */
+    multiplyScalar: function (f) {
+        return new Quaternion(this.w * f, this.x * f, this.y * f, this.z * f);
+    },
+    
+    /**
+    * Converts the quaternion to a rotation matrix
+    * @return {Matrix4x4}
+    */
+    toRotationMatrix: function () {
+        var x, y, z, wx, wy, wz, xx, xy, xz, yy, yz, zz;
+        x = 2.0 * this.x;
+        y = 2.0 * this.y;
+        z = 2.0 * this.z;
+        wx = x * this.w;
+        wy = y * this.w;
+        wz = z * this.w;
+        xx = x * this.x;
+        xy = y * this.x;
+        xz = z * this.x;
+        yy = y * this.y;
+        yz = z * this.y;
+        zz = z * this.z;
+        
+        //TODO: When Matrix3x3 exists change this to a 3x3 instead
+        return new Matrix4x4(1.0 - (yy + zz), xy - wz, xz + wy, 0,
+                             xy + wz, 1.0 - (xx + zz), yz - wx, 0,
+                             xz - wy, yz + wx, 1.0 - (xx + yy), 0,
+                             0, 0, 0, 1);
+    },
+    
+    /**
+    * Converts the Quaternion to an axis and an angle
+    * @return {Vector4} Containing the values x,y,z,angle where angle is in radians
+    */
+    toAxisAngle: function () {
+        var lengthSquared, inverseLength;
+        
+        lengthSquared = this.x * this.x + this.y * this.y + this.z * this.z;
+        if (lengthSquared > MathHelper.zeroTolerance)
+        {
+            inverseLength = MathHelper.invSqrt(lengthSquared);
+            return new Vector4(this.x * inverseLength, this.y * inverseLength, this.z * inverseLength, 2.0 * MathHelper.acos(this.w));
+        }
+        return new Vector4(1, 0, 0, 0);
+    },
+    
+    /**
+    * Returns a string containing the current state of the Quaternion
+    * @return {String}
+    */
+    toString: function () {
+        return '[' + this.w + ', ' + this.x + ', ' + this.y + ', ' + this.z + ']';
+    }
+};
---
+++ src/Math/tests/QuaternionTest.js
@@ -0,0 +1,204 @@
+module("Math::Quaternion");
+
+test("Constructor", function() {
+    var q = new Quaternion(1.1,2.2,3.3,4.4);
+	equals(q.w, 1.1, "w");
+    equals(q.x, 2.2, "x");
+    equals(q.y, 3.3, "y");
+    equals(q.z, 4.4, "z");
+});
+
+test("fromRotationMatrix", function() {
+    var m = Matrix4x4.rotateX(MathHelper.PI / 2);
+    var q = Quaternion.fromRotationMatrix(m);
+    var v = new Vector3(0,1,0);
+    var v2 = q.transform(v);
+    
+    ok(MathHelper.abs(v2.x - 0) <= MathHelper.zeroTolerance, "x1");
+    ok(MathHelper.abs(v2.y - 0) <= MathHelper.zeroTolerance, "y1");
+    ok(MathHelper.abs(v2.z - 1) <= MathHelper.zeroTolerance, "z1");
+    
+    m = Matrix4x4.rotateY(MathHelper.PI / 2);
+    q = Quaternion.fromRotationMatrix(m);
+    v = new Vector3(1,0,0);
+    v2 = q.transform(v);
+    ok(MathHelper.abs(v2.x - 0) <= MathHelper.zeroTolerance, "x2");
+    ok(MathHelper.abs(v2.y - 0) <= MathHelper.zeroTolerance, "y2");
+    ok(MathHelper.abs(v2.z - 1) <= MathHelper.zeroTolerance, "z2");
+    
+    m = Matrix4x4.rotateZ(MathHelper.PI / 2);
+    q = Quaternion.fromRotationMatrix(m);
+    v = new Vector3(0,1,0);
+    v2 = q.transform(v);
+    ok(MathHelper.abs(v2.x - -1) <= MathHelper.zeroTolerance, "x3");
+    ok(MathHelper.abs(v2.y - 0) <= MathHelper.zeroTolerance, "y3");
+    ok(MathHelper.abs(v2.z - 0) <= MathHelper.zeroTolerance, "z3");
+});
+
+test("fromAxisAngle", function() {
+    var v = new Vector3(1.1, 2.2, 3.3);
+    v = v.normalize();
+    
+    var q = Quaternion.fromAxisAngle(v, MathHelper.PI / 3);
+    equals(q.w, 0.8660254037844387, "w");
+    equals(q.x, 0.1336306209562122, "x");
+    equals(q.y, 0.2672612419124244, "y");
+    equals(q.z, 0.40089186286863654, "z");
+});
+
+test("dot", function() {
+    var q1 = new Quaternion(1.1, -2.2, 3.3, -4.4);
+    var q2 = new Quaternion(5.5, 6.6, -7.7, 8.8);
+    var d = q1.dot(q2);
+    equals(d, -72.6, "d");
+});
+
+test("inverse", function() {
+
+    var axis = new Vector3(1,0,0);
+    var q = Quaternion.fromAxisAngle(axis, (MathHelper.PI / 2.0));
+    var v = new Vector3(0,1,0);
+    var v2 = q.transform(v);
+    
+    var qInv = q.inverse();
+    v2 = qInv.transform(v2);
+    ok(MathHelper.abs(v2.x - 0) <= MathHelper.zeroTolerance, "x1");
+    ok(MathHelper.abs(v2.y - 1) <= MathHelper.zeroTolerance, "y1");
+    ok(MathHelper.abs(v2.z - 0) <= MathHelper.zeroTolerance, "z1");
+});
+
+test("conjugate", function() {
+    var q1 = new Quaternion(-4.4, 1.1,-2.2,3.3);
+    var q = q1.conjugate();
+    
+	equals(q.w, -4.4, "w");
+    equals(q.x, -1.1, "x");
+    equals(q.y, 2.2, "y");
+    equals(q.z, -3.3, "z");
+});
+
+test("slerp", function() {
+    var qSource = Quaternion.fromAxisAngle(new Vector3(1,0,0), 0);
+    var qTarget = Quaternion.fromAxisAngle(new Vector3(3,5,9), MathHelper.PI / 3);
+    var q = Quaternion.slerp(0.56, qSource, qTarget); 
+    equals(q.w, 0.9573194975320675, "w");
+    equals(q.x, 0.8670953908334149, "x");
+    equals(q.y, 1.445158984722358, "y");
+    equals(q.z, 2.6012861725002447, "z");
+    
+    q = Quaternion.slerp(0, qSource, qTarget); 
+    equals(q.w, qSource.w, "w");
+    equals(q.x, qSource.x, "x");
+    equals(q.y, qSource.y, "y");
+    equals(q.z, qSource.z, "z");
+    
+    q = Quaternion.slerp(1, qSource, qTarget); 
+    equals(q.w, qTarget.w, "w");
+    equals(q.x, qTarget.x, "x");
+    equals(q.y, qTarget.y, "y");
+    equals(q.z, qTarget.z, "z");
+});
+
+test("add", function() {
+    var q1 = new Quaternion(1.1, -2.2, 3.3, -4.4);
+    var q2 = new Quaternion(3.5, 9.6, -2.7, 5.8);
+    var q = q1.add(q2);
+    equals(q.w, 4.6, "w");
+    equals(q.x, 7.3999999999999995, "x");
+    equals(q.y, 0.5999999999999996, "y");
+    equals(q.z, 1.3999999999999995, "z");
+});
+
+test("length", function() {
+    var q1 = new Quaternion(-4.4, 1.1,-2.2,3.3);
+    
+	ok(MathHelper.abs(q1.length() - MathHelper.sqrt(-4.4 * -4.4 + 1.1 * 1.1 + -2.2 * -2.2 + 3.3 * 3.3)) <= MathHelper.zeroTolerance);
+});
+
+test("normalize", function() {
+    var q1 = new Quaternion(-4.4, 1.1,-2.2,3.3);
+    var q = q1.normalize();
+    equals(q.w, -0.7302967433402214, "w");
+    equals(q.x, 0.18257418583505536, "x");
+    equals(q.y, -0.3651483716701107, "y");
+    equals(q.z, 0.547722557505166, "z");
+});
+
+test("transform", function() {
+    var axis = new Vector3(1,0,0);
+    var q = Quaternion.fromAxisAngle(axis, (MathHelper.PI / 2.0));
+    var v = new Vector3(0,1,0);
+    var v2 = q.transform(v);
+    ok(MathHelper.abs(v2.x - 0) <= MathHelper.zeroTolerance, "x1");
+    ok(MathHelper.abs(v2.y - 0) <= MathHelper.zeroTolerance, "y1");
+    ok(MathHelper.abs(v2.z - 1) <= MathHelper.zeroTolerance, "z1");
+    
+    axis = new Vector3(0,1,0);
+    q = Quaternion.fromAxisAngle(axis, (MathHelper.PI / 2.0));
+    v = new Vector3(1,0,0);
+    v2 = q.transform(v);
+    ok(MathHelper.abs(v2.x - 0) <= MathHelper.zeroTolerance, "x2");
+    ok(MathHelper.abs(v2.y - 0) <= MathHelper.zeroTolerance, "y2");
+    ok(MathHelper.abs(v2.z - -1) <= MathHelper.zeroTolerance, "z2");
+    
+    axis = new Vector3(0,0,-1);
+    q = Quaternion.fromAxisAngle(axis, (MathHelper.PI / 2.0));
+    v = new Vector3(0,1,0);
+    v2 = q.transform(v);
+    ok(MathHelper.abs(v2.x - 1) <= MathHelper.zeroTolerance, "x3");
+    ok(MathHelper.abs(v2.y - 0) <= MathHelper.zeroTolerance, "y3");
+    ok(MathHelper.abs(v2.z - 0) <= MathHelper.zeroTolerance, "z3");
+});
+
+test("multiplyScalar", function() {
+    var q1 = new Quaternion(-4.4, 1.1,-2.2,3.3);
+    var q = q1.multiplyScalar(3.2);
+	equals(q.w, -4.4 * 3.2, "w");
+    equals(q.x, 1.1 * 3.2, "x");
+    equals(q.y, -2.2 * 3.2, "y");
+    equals(q.z, 3.3 * 3.2, "z");
+});
+
+test("multiply", function() {
+    var q1 = new Quaternion(-4.4, 1.1, -2.2, 3.3);
+    var q2 = new Quaternion(8.8, 5.5, 6.6, -7.7);
+    var q = q2.multiply(q1);
+    q = q.normalize();
+	equals(q.w, -0.05536365323582676, "w");
+    equals(q.x, -0.11072730647165334, "x");
+    equals(q.y, -0.858136625155313, "y");
+    equals(q.z, 0.4982728791224398, "z");
+});
+
+test("toRotationMatrix", function() {
+    var q1 = new Quaternion(-4.4, 1.1, -2.2, 3.3);
+    var m = q1.toRotationMatrix();
+	equals(m.m11, -30.46, "m11 constructor");
+    equals(m.m12, 24.2, "m12 constructor");
+    equals(m.m13, 26.620000000000005, "m13 constructor");
+    equals(m.m14, 0, "m14 constructor");
+    equals(m.m21, -33.88, "m21 constructor");
+    equals(m.m22, -23.2, "m22 constructor");
+    equals(m.m23, -4.839999999999998, "m23 constructor");
+    equals(m.m24, 0, "m24 constructor");
+    equals(m.m31, -12.100000000000003, "m31 constructor");
+    equals(m.m32, -24.200000000000003, "m32 constructor");
+    equals(m.m33, -11.100000000000001, "m33 constructor");
+    equals(m.m34, 0, "m34 constructor");
+    equals(m.m41, 0, "m41 constructor");
+    equals(m.m42, 0, "m42 constructor");
+    equals(m.m43, 0, "m43 constructor");
+    equals(m.m44, 1, "m44 constructor");
+});
+
+test("toAxisAngle", function() {
+    var v = new Vector3(1,2,3);
+    v = v.normalize();
+    var q = Quaternion.fromAxisAngle(v, MathHelper.PI / 3);
+    
+    var axisAngle = q.toAxisAngle();
+    ok(MathHelper.abs(axisAngle.w - MathHelper.PI / 3) <= MathHelper.zeroTolerance, "w");
+    ok(MathHelper.abs(axisAngle.x - v.x) <= MathHelper.zeroTolerance, "x");
+    ok(MathHelper.abs(axisAngle.y - v.y) <= MathHelper.zeroTolerance, "y");
+    ok(MathHelper.abs(axisAngle.z - v.z) <= MathHelper.zeroTolerance, "z");
+});
