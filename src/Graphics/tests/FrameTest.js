5888a63e768ce656134d29cbbc6289618dcc77c8
Mark Dawson <markdawson@live.com>
2010-09-19 17:46:21 -0700

    Adding HTML5 doctype to asteroids index page so ie9 turns on canvas feature

--- src/Graphics/tests/FrameTest.js
+++ src/Graphics/tests/FrameTest.js
@@ -2,11 +2,11 @@ module("Graphics::Frame");
 
 test("Constructor", function() {
     var f = new Frame();
-	equals(f.Position.x, 0, "x");
-    equals(f.Position.y, 0, "y");
-    equals(f.Position.z, 0, "z");
-    equals(f.Rotation.w, 1, "rw");
-    equals(f.Rotation.x, 0, "rx");
-    equals(f.Rotation.y, 0, "ry");
-    equals(f.Rotation.z, 0, "rz");
+	equals(f.position.x, 0, "x");
+    equals(f.position.y, 0, "y");
+    equals(f.position.z, 0, "z");
+    equals(f.rotation.w, 1, "rw");
+    equals(f.rotation.x, 0, "rx");
+    equals(f.rotation.y, 0, "ry");
+    equals(f.rotation.z, 0, "rz");
 });
--- src/Graphics/tests/SpriteBatchTest.js
+++ src/Graphics/tests/SpriteBatchTest.js
@@ -1,12 +1,4 @@
 module("Graphics::SpriteBatch");
 
 test("Constructor", function() {
-    
-    
-    var s = new SpriteBatch(device);
-    
-    s.begin();
-    
-    var d = new SpriteDrawOptions()
-    s.end();
 });
